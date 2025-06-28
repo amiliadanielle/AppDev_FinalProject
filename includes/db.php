a10f93b02a392affea0b413d83544ad2ca02aa89
ashklve <hilaryashleypagador@gmail.com>
2025-06-28 22:42:16 +0800

    add database connection setup and remove unused header and footer files

---
+++ includes/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$dbname = "juno_hotel_db";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
